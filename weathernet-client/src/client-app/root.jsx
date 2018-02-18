debbf8f209f0c214f3f6399180b45b685104722d
unknown <Ryan McIntire>
2018-02-18 12:27:52 -0800

    Wire up weather display contianer in client.

--- weathernet-client/src/client-app/root.jsx
+++ weathernet-client/src/client-app/root.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import Title from './components/title';
 import Search from './components/search';
+import WeatherDisplayContainer from './components/weather-display/weather-display-container';
 
 export default class Root extends React.Component {
   render() {
@@ -14,6 +15,11 @@ export default class Root extends React.Component {
         </div>
         <div className="row">
           <div className="container"><Search /></div>
+        </div>
+        <div className="row">
+          <div className="container">
+            <WeatherDisplayContainer />
+          </div>
         </div>
       </div>
     );
