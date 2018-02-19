a6f589444f8c7b0a8a814e07b4a72ee557dbf03c
unknown <Ryan McIntire>
2018-02-18 16:07:46 -0800

    Cleanup reducer.

--- weathernet-client/src/client-app/reducers/weather-reducer.js
+++ weathernet-client/src/client-app/reducers/weather-reducer.js
@@ -23,16 +23,3 @@ export default (state = initialState, action) => {
       return state;
   }
 }
-
-// export default function(state = {}, action) {
-//   switch (action.type) {
-//     case GET_WEATHER: 
-//       console.log('In weather reducer');
-//       return {
-//         ...state,
-//         weather: action.payload.data
-//       }
-//     default: 
-//       return state;
-//   }
-// }
